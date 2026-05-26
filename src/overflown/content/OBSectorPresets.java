a2225c6b34fcd047a47c0e03148a0adf996bb29f
Cubical box <calreibozsemimapping@gmail.com>
2026-05-26 15:26:29 +0800

    cooked

--- src/overflown/content/OBSectorPresets.java
+++ src/overflown/content/OBSectorPresets.java
@@ -6,11 +6,12 @@ import mindustry.type.*;
 import static mindustry.content.Planets.*;
 
 public class OBSectorPresets{
-  public static SectorPreset lifelessCanyon;
+   public static SectorPreset lifelessCanyon;
 
-  public static void load(){
-    lifelessCanyon = new SectorPreset("lifelessCanyon", serpulo, 256){{
-      captureWave = 30;
-      difficulty = 3;
-  }}
+   public static void load(){
+      lifelessCanyon = new SectorPreset("lifelessCanyon", serpulo, 256){{
+         captureWave = 30;
+         difficulty = 3;
+      }}
+   }
 }
