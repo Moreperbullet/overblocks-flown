ca8c33eacc5ed8f5179ceaccba1a5a14a18a6615
Cubical box <calreibozsemimapping@gmail.com>
2026-06-16 20:29:33 +0800

    update DensityProjector.java

--- src/overflown/world/blocks/DensityProjector.java
+++ src/overflown/world/blocks/DensityProjector.java
@@ -1,17 +1,50 @@
 package overflown.world.blocks;
 
 import arc.*;
+import arc.audio.*;
+import arc.func.*;
 import arc.graphics.*;
 import arc.graphics.g2d.*;
 import arc.math.*;
+import arc.math.geom.*;
+import arc.struct.*;
 import arc.util.*;
 import arc.util.io.*;
+import mindustry.content.*;
+import mindustry.entities.*;
+import mindustry.game.*;
 import mindustry.gen.*;
-import mindustry.world.blocks.defense.*;
+import mindustry.graphics.*;
+import mindustry.logic.*;
+import mindustry.type.*;
+import mindustry.ui.*;
+import mindustry.world.*;
+import mindustry.world.blocks.*;
+import mindustry.world.consumers.*;
+import mindustry.world.meta.*;
+
 
 public class DensityProjector extends ForceProjector{
    public int phases = 4;
    //TODO armor
+
+   public ForceProjector(String name){
+      super(name);
+      update = true;
+      solid = true;
+      group = BlockGroup.projectors;
+      hasPower = true;
+      hasLiquids = true;
+      hasItems = true;
+      envEnabled |= Env.space;
+      ambientSound = Sounds.loopShield;
+      ambientSoundVolume = 0.1f;
+      flags = EnumSet.of(BlockFlag.shield);
+
+      if(consumeCoolant){
+         consume(coolantConsumer = new ConsumeCoolant(coolantConsumption)).boost().update(false);
+      }
+   }
 
    @Override
    public void setBars(){
@@ -19,7 +52,7 @@ public class DensityProjector extends ForceProjector{
 
       addBar("shieldphase", (DensityBuild entity) ->
       new Bar(() ->
-      Core.bundle.format("bar.shield-phases", entity.currentPhase), () -> Color.scarlet, () -> entity.currentPhase / phases));
+      Core.bundle.format("bar.shield-phases", entity.currentPhase), () -> Pal.darkMetal, () -> entity.currentPhase / phases));
    }
 
    public class DensityBuild extends ForceBuild{
