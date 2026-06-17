f5a90261873f9e6d2a69cf3e3581b12fee5f5e1b
Cubical box <calreibozsemimapping@gmail.com>
2026-06-17 15:52:36 +0800

    update ReflectionWall.java

--- src/overflown/world/blocks/ReflectionWall.java
+++ src/overflown/world/blocks/ReflectionWall.java
@@ -3,6 +3,7 @@ package overflown.world.blocks;
 import arc.*;
 import mindustry.gen.*;
 import mindustry.world.blocks.defense.*;
+import mindustry.world.meta.*;
 import overflown.world.meta.*;
 
 public class ReflectionWall extends Wall{
@@ -17,16 +18,16 @@ public class ReflectionWall extends Wall{
         super.setStats();
         stats.add(OBStats.special, Core.bundle.get("special-reflection"));
         stats.addPercent(OBStats.reflectModNum, reflectMod);
-        stats.add(OBStats.reflectModCap, reflectCap, StatUnit.none);
+        stats.add(OBStats.reflectCapNum, reflectCap, StatUnit.none);
     }
 
     public class ReflectionWallBuild extends WallBuild{
         @Override
         public boolean collision(Bullet bullet){
             super.collision(bullet);
-            float finalDamage = Math.min(bulletDamage() * reflectMod, reflectCap);
+            float finalDamage = Math.min(bullet.damage() * reflectMod, reflectCap);
 
-            if(damageMultiplier > 0f && bullet.owner instanceof Healthc c) {
+            if(finalDamage > 0f && bullet.owner instanceof Healthc c) {
                 c.damage(finalDamage);
             }
             return true;
