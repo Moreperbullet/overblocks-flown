316ff17ede3422fc759d491a76d22c066b1e6837
Moreperbullet <calreibozsemimapping@gmail.com>
2026-07-31 19:28:51 +0800

    will this work

---
+++ src/overflown/io/OBTypeIO.java
@@ -0,0 +1,27 @@
+package overflown.io;
+
+import arc.util.*;
+import arc.util.io.*;
+import ent.anno.Annotations.*;
+import mindustry.entities.abilities.*;
+import overflown.entities.abilities.*;
+
+@TypeIOHandler
+public class OBTypeIo{
+    
+    public static void writeInvisibleA(Writes write
+, Ability[] abilities, InvisibleAbility invisibleA){
+        if(invisibleA != null && Structs.contains(abilities, invisibleA)){
+	    write.b(1);
+	}else{
+	    write.b(0);
+	}
+    }
+
+    public static InvisibleAbility readInvisibleA(Reads read, Ability[] abilities){
+	byte existence
+	if(existence = 1){
+	    for(var a : abilities) if(a instanceof InvisibleAbility i) return i;
+	}
+    }
+}
